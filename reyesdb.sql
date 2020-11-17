f2357be0ee7ed236b06504af197c152e1f65fed6
Mark Kennedy <kennedymr18@gmail.com>
2020-11-17 13:30:05 -0500

    First code

---
+++ reyesdb.sql
@@ -0,0 +1,237 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 17, 2020 at 04:11 PM
+-- Server version: 8.0.18
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `reyesdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blockedusers`
+--
+
+CREATE TABLE `blockedusers` (
+  `Date` date NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `UserID2` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `colleagues`
+--
+
+CREATE TABLE `colleagues` (
+  `Date` date NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `UserID2` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `StatusID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `Content` longtext COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `Date` date NOT NULL,
+  `Time` time NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `UserID2` int(11) NOT NULL,
+  `Content` longtext COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `UserID` int(11) NOT NULL,
+  `UserID2` int(11) NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL,
+  `Reason` longtext COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statuses`
+--
+
+CREATE TABLE `statuses` (
+  `StatusID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `Date` date NOT NULL,
+  `Time` time NOT NULL,
+  `Content` longtext COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` int(11) NOT NULL,
+  `FirstName` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `LastName` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `Email` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `Password` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `State` varchar(2) COLLATE utf8mb4_general_ci NOT NULL,
+  `City` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `YearsLicensed` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `Picture` longblob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `blockedusers`
+--
+ALTER TABLE `blockedusers`
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `UserID2` (`UserID2`);
+
+--
+-- Indexes for table `colleagues`
+--
+ALTER TABLE `colleagues`
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `UserID2` (`UserID2`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD KEY `StatusID` (`StatusID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `UserID2` (`UserID2`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `UserID2` (`UserID2`);
+
+--
+-- Indexes for table `statuses`
+--
+ALTER TABLE `statuses`
+  ADD PRIMARY KEY (`StatusID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `statuses`
+--
+ALTER TABLE `statuses`
+  MODIFY `StatusID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `blockedusers`
+--
+ALTER TABLE `blockedusers`
+  ADD CONSTRAINT `blockedusers_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `blockedusers_ibfk_2` FOREIGN KEY (`UserID2`) REFERENCES `users` (`UserID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `colleagues`
+--
+ALTER TABLE `colleagues`
+  ADD CONSTRAINT `colleagues_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `colleagues_ibfk_2` FOREIGN KEY (`UserID2`) REFERENCES `users` (`UserID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`StatusID`) REFERENCES `statuses` (`StatusID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`UserID2`) REFERENCES `users` (`UserID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `reports_ibfk_2` FOREIGN KEY (`UserID2`) REFERENCES `users` (`UserID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `statuses`
+--
+ALTER TABLE `statuses`
+  ADD CONSTRAINT `statuses_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
