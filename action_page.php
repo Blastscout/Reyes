9afdf7893a45e8a2377b174b3524b1b840bfc1bf
Julian <Juliancuttino@gmail.com>
2020-11-17 15:54:09 -0500

    Adjusted pathfile in html. Added php to add user

---
+++ action_page.php
@@ -0,0 +1,36 @@
+<html>
+<body>
+
+<?php
+//connection
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+$dbname = "reyesdb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn->connect_error){
+	die("Connection Failed : " .$conn->connect_error);
+	}
+//creates variables
+$firstName = $_POST["fname"];
+$lastName = $_POST["lname"];
+$email = $_POST["email"];
+$state = $_POST["state"];
+$city = $_POST["city"];
+$years = $_POST["years"];
+$password = $_POST["psw"];
+
+$sql = "INSERT INTO users(FirstName,LastName,Email,Password,State,City,YearsLicensed)VALUES('$firstName','$lastName','$email','$password','$state','$city','$years')";
+if ($conn->query($sql) === TRUE) {
+ echo "Sign up successfully!";
+ header("location: index.html");
+}
+else {
+ echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+?>
+
+</body>
+</html>
