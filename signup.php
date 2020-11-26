c3768a56ee1d34c04555269cad256e3c7c0d56b8
Julian <Cuttinoj@go.stockton.edu>
2020-11-25 19:55:10 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -22,6 +22,16 @@ $years = $_POST["years"];
 $pass = $_POST["psw"];
 //Hashes password
 //$hashed = password_hash($pass, PASSWORD_DEFAULT);
+$table = "SELECT * from users where Email= '$email'";
+$result = mysqli_query($conn, $table);
+$count = mysqli_num_rows($result);
+if($count > 0){
+	echo"
+	<script> alert('Email address is taken. Enter a different one or login');
+	window.location = 'SIGN-UPPAGE.html';
+	</script>";
+}
+else{
 $sql = "INSERT INTO users(FirstName,LastName,Email,Password,State,City,YearsLicensed)VALUES('$firstName','$lastName','$email','$pass','$state','$city','$years')";
 if ($conn->query($sql) === TRUE) {
  echo "Sign up successfully!";
@@ -31,6 +41,7 @@ else {
  echo "Error: " . $sql . "<br>" . $conn->error;
 }
 $conn->close();
+}
 ?>
 
 </body>
