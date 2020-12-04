fda0d0f63e6a3e7091d6e817a2b5d8fbae1f54bd
Mark Kennedy <45669536+Blastscout@users.noreply.github.com>
2020-12-04 11:09:47 -0500

    Updated Variables

--- notifications.php
+++ notifications.php
@@ -5,14 +5,14 @@ if($user_ok != true || $log_username == ""){
     exit();
 }
 $notification_list = "";
-$sql = "SELECT * FROM notifications WHERE username LIKE BINARY '$log_username' ORDER BY date_time DESC";
+$sql = "SELECT * FROM notifications WHERE UserID LIKE BINARY '$log_username' ORDER BY date_time DESC";
 $query = mysqli_query($db_conx, $sql);
 $numrows = mysqli_num_rows($query);
 if($numrows < 1){
 	$notification_list = "You do not have any notifications";
 } else {
 	while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
-		$noteid = $row["id"];
+		$noteid = $row["ID"];
 		$initiator = $row["initiator"];
 		$app = $row["app"];
 		$note = $row["note"];
@@ -21,34 +21,34 @@ if($numrows < 1){
 		$notification_list .= "<p><a href='user.php?u=$initiator'>$initiator</a> | $app<br />$note</p>";
 	}
 }
-mysqli_query($db_conx, "UPDATE users SET notescheck=now() WHERE username='$log_username' LIMIT 1");
+mysqli_query($db_conx, "UPDATE users SET notescheck=now() WHERE UserID='$log_username' LIMIT 1");
 ?><?php
-$friend_requests = "";
-$sql = "SELECT * FROM friends WHERE user2='$log_username' AND accepted='0' ORDER BY datemade ASC";
+$colleague_requests = "";
+$sql = "SELECT * FROM colleagues WHERE UserID2='$log_username' AND accepted='0' ORDER BY Datemade ASC";
 $query = mysqli_query($db_conx, $sql);
 $numrows = mysqli_num_rows($query);
 if($numrows < 1){
-	$friend_requests = 'No friend requests';
+	$colleague_requests = 'No colleague requests';
 } else {
 	while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
-		$reqID = $row["id"];
-		$user1 = $row["user1"];
-		$datemade = $row["datemade"];
+		$reqID = $row["ID"];
+		$user1 = $row["UserID"];
+		$datemade = $row["Datemade"];
 		$datemade = strftime("%B %d", strtotime($datemade));
-		$thumbquery = mysqli_query($db_conx, "SELECT avatar FROM users WHERE username='$user1' LIMIT 1");
+		$thumbquery = mysqli_query($db_conx, "SELECT picture FROM users WHERE UserID='$user1' LIMIT 1");
 		$thumbrow = mysqli_fetch_row($thumbquery);
-		$user1avatar = $thumbrow[0];
-		$user1pic = '<img src="user/'.$user1.'/'.$user1avatar.'" alt="'.$user1.'" class="user_pic">';
-		if($user1avatar == NULL){
-			$user1pic = '<img src="images/avatardefault.jpg" alt="'.$user1.'" class="user_pic">';
+		$user1picture = $thumbrow[0];
+		$user1pic = '<img src="user/'.$user1.'/'.$user1picture.'" alt="'.$user1.'" class="user_pic">';
+		if($user1picture == NULL){
+			$user1pic = '<img src="images/demo/avatar.png" alt="'.$user1.'" class="user_pic">';
 		}
-		$friend_requests .= '<div id="friendreq_'.$reqID.'" class="friendrequests">';
-		$friend_requests .= '<a href="user.php?u='.$user1.'">'.$user1pic.'</a>';
-		$friend_requests .= '<div class="user_info" id="user_info_'.$reqID.'">'.$datemade.' <a href="user.php?u='.$user1.'">'.$user1.'</a> requests friendship<br /><br />';
-		$friend_requests .= '<button onclick="friendReqHandler(\'accept\',\''.$reqID.'\',\''.$user1.'\',\'user_info_'.$reqID.'\')">accept</button> or ';
-		$friend_requests .= '<button onclick="friendReqHandler(\'reject\',\''.$reqID.'\',\''.$user1.'\',\'user_info_'.$reqID.'\')">reject</button>';
-		$friend_requests .= '</div>';
-		$friend_requests .= '</div>';
+		$colleague_requests .= '<div id="colleaguereq_'.$reqID.'" class="colleaguerequests">';
+		$colleague_requests .= '<a href="user.php?u='.$user1.'">'.$user1pic.'</a>';
+		$colleague_requests .= '<div class="user_info" id="user_info_'.$reqID.'">'.$datemade.' <a href="user.php?u='.$user1.'">'.$user1.'</a> requests colleagueship<br /><br />';
+		$colleague_requests .= '<button onclick="colleagueReqHandler(\'accept\',\''.$reqID.'\',\''.$user1.'\',\'user_info_'.$reqID.'\')">accept</button> or ';
+		$colleague_requests .= '<button onclick="colleagueReqHandler(\'reject\',\''.$reqID.'\',\''.$user1.'\',\'user_info_'.$reqID.'\')">reject</button>';
+		$colleague_requests .= '</div>';
+		$colleague_requests .= '</div>';
 	}
 }
 ?>
@@ -56,32 +56,32 @@ if($numrows < 1){
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Notifications and Friend Requests</title>
+<title>Notifications and Colleague Requests</title>
 <link rel="icon" href="favicon.ico" type="image/x-icon">
 <link rel="stylesheet" href="style/style.css">
 <style type="text/css">
 div#notesBox{float:left; width:430px; border:#F0F 1px dashed; margin-right:60px; padding:10px;}
-div#friendReqBox{float:left; width:430px; border:#F0F 1px dashed; padding:10px;}
-div.friendrequests{height:74px; border-bottom:#CCC 1px solid; margin-bottom:8px;}
+div#colleagueReqBox{float:left; width:430px; border:#F0F 1px dashed; padding:10px;}
+div.colleaguerequests{height:74px; border-bottom:#CCC 1px solid; margin-bottom:8px;}
 img.user_pic{float:left; width:68px; height:68px; margin-right:8px;}
 div.user_info{float:left; font-size:14px;}
 </style>
 <script src="js/main.js"></script>
 <script src="js/ajax.js"></script>
 <script type="text/javascript">
-function friendReqHandler(action,reqid,user1,elem){
-	var conf = confirm("Press OK to '"+action+"' this friend request.");
+function colleagueReqHandler(action,reqid,user1,elem){
+	var conf = confirm("Press OK to '"+action+"' this colleague request.");
 	if(conf != true){
 		return false;
 	}
 	_(elem).innerHTML = "processing ...";
-	var ajax = ajaxObj("POST", "php_parsers/friend_system.php");
+	var ajax = ajaxObj("POST", "php_parsers/COLLEAGUE_system.php");
 	ajax.onreadystatechange = function() {
 		if(ajaxReturn(ajax) == true) {
 			if(ajax.responseText == "accept_ok"){
-				_(elem).innerHTML = "<b>Request Accepted!</b><br />Your are now friends";
+				_(elem).innerHTML = "<b>Request Accepted!</b><br />Your are now colleagues";
 			} else if(ajax.responseText == "reject_ok"){
-				_(elem).innerHTML = "<b>Request Rejected</b><br />You chose to reject friendship with this user";
+				_(elem).innerHTML = "<b>Request Rejected</b><br />You chose to reject colleagueship with this user";
 			} else {
 				_(elem).innerHTML = ajax.responseText;
 			}
@@ -96,7 +96,7 @@ function friendReqHandler(action,reqid,user1,elem){
 <div id="pageMiddle">
   <!-- START Page Content -->
   <div id="notesBox"><h2>Notifications</h2><?php echo $notification_list; ?></div>
-  <div id="friendReqBox"><h2>Friend Requests</h2><?php echo $friend_requests; ?></div>
+  <div id="colleagueReqBox"><h2>Friend Requests</h2><?php echo $colleague_requests; ?></div>
   <div style="clear:left;"></div>
   <!-- END Page Content -->
 </div>
