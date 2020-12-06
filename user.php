5f98cd567d9953014f38be4624361e18f6084d71
milesogor <74623331+milesogor@users.noreply.github.com>
2020-12-06 13:18:04 -0500

    Update user.php

--- user.php
+++ user.php
@@ -49,7 +49,8 @@ while ($row = mysqli_fetch_array($user_query, MYSQLI_ASSOC)) {
 	$city = $row["City"];
 	$yearslicensed = $row["YearsLincensed"];
 }
-?><?php
+?>
+<?php
 $isColleague = false;
 $ownerBlockViewer = false;
 $viewerBlockOwner = false;
@@ -131,8 +132,117 @@ if($colleague_count < 1){
 }
 ?>
 <!DOCTYPE html>
-<html>
+
+<html lang="">
 <head>
+<title>REYES REAL ESTATE NETWORK</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<style>
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+</style>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row0">
+  <div id="topbar" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div class="fl_left">
+      <ul class="nospace">
+        <li><a href="index.html"><i class="fas fa-home fa-lg"></i></a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="mailto:ogorzalm@go.stockton.edu">Contact</a></li>
+        <li><a href=LOGIN.html>Login</a></li>
+        <li><a href=SIGN-UPPAGE.html>Register</a></li>
+		<li><a href=complaintForm.html>File Complaint</a></li>
+      </ul>
+    </div>
+    <div class="fl_right">
+      <ul class="nospace">
+        <li><i class="fas fa-phone rgtspace-5"></i> +00 (123) 456 7890</li>
+        <li><i class="fas fa-envelope rgtspace-5"></i> <a href="mailto:ogorzalm@go.stockton.edu">ogorzalm@go.stockton.edu</a></li>
+      </ul>
+    </div>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row1">
+  <header id="header" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div id="logo" class="one_quarter first">
+      <h1><a href="index.html"><b>REYES REAL ESTATE SOCIAL NETWORK</b></a></h1>
+      <p>Meet the perfect agent for you.</p>
+    </div>
+    <div class="one_quarter"><strong><i class="fas fa-phone rgtspace-5"></i> Call Us:</strong> +00 (123) 456 7890</div>
+    <div class="one_quarter"><strong><i class="far fa-clock rgtspace-5"></i> Mon. - Sat.:</strong> 08:00am - 18:00pm</div>
+    <div class="one_quarter">
+      <form action="#" method="post">
+        <label>
+          <select>
+            <option value="" selected="selected" disabled="disabled">Language</option>
+            <option value="English">Default</option>
+            <option value="Spanish">Spanish</option>
+          </select>
+        </label>
+      </form>
+    </div>
+    <!-- ################################################################################################ -->
+  </header>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row2">
+  <nav id="mainav" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <ul class="clear">
+      <li class="active"><a href="index.html">Home</a></li>
+      <li><a class="drop" href="#">Explore</a>
+        <ul>
+          <li><a href=BROWSEAGENTS.php>Browse Agents</a></li>
+          <li><a href=ABOUT.html>What Does this Site Do?</a></li>
+          <li><a href=TUTORIAL.html>Get Started Here</a></li>
+          <li><a href=CONTACT.html>Our Socia Medias</a></li>
+          <li><a href=CONTACT.html>Contact Us </a></li>
+        </ul>
+      </li>
+      <li><a class="drop" href="#">Browse Agents</a>
+        <ul>
+          <li><a href=BROWSEAGENTS.php>Browse Agents...</a></li>
+          <li><a class="drop" href="#">Browse Agents By Filter...</a>
+            <ul>
+              <li><a href=BROWSEAGENTSLOCATION.html>By Location</a></li>
+              <li><a href=BROWSEAGENTSRATING.html>By Rating</a></li>
+              <li><a href=BROWSEAGENTSEXP.html>By Years of Experience</a></li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+      <li><a href="#">Get Started Here</a></li>
+      <li><a href="#">Our Socia Medias</a></li>
+      <li><a href="#">What Does this Site Do?</a></li>
+    </ul>
+    <!-- ################################################################################################ -->
+  </nav>
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="content"> 
+<h1><u>Your Profile</u></h1>
 <meta charset="UTF-8">
 <title><?php echo $u; ?></title>
 <link rel="icon" href="favicon.ico" type="image/x-icon">
@@ -201,6 +311,16 @@ function blockToggle(type,blockee,elem){
   <p>Block Button: <span id="blockBtn"><?php echo $block_button; ?></span></p>
   <hr />
   <p><?php echo $colleaguesHTML; ?></p>
+ </div>
+ 
 </div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fas fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
 </body>
 </html>
