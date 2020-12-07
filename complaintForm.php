f3a7d63fe5dddd14d1c509d79e018fd0b6230eeb
JulianCuttino <juliancuttino@gmail.com>
2020-12-06 19:31:18 -0500

    Create complaintForm.php

---
+++ complaintForm.php
@@ -0,0 +1,33 @@
+<html>
+<body>
+
+<?php
+//connection
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+$dbname = "reyesdb";
+
+$conn = new mysql($servername, $username, $password, $dbname);
+if($conn->connect_error){
+	die("Connection Failed : " .$conn->connect_error);
+	}
+//create variables
+$name = $_POST["UserID"];
+$complaineeName = $_POST["UserID2"];
+$date = $_POST["DateMade"];
+$reason = $_POST["Reason"];
+
+$sql = "INSERT INTO reports(UserID,UserID2,DateMade,Reason)VALUES ('$name','$complaineeName','$date','$reason')";
+if ($conn->query($sql) === TRUE) {
+   echo "<script> alert('Complaint was filed successfully.');
+   window.location = 'index.html';
+   </script>";
+
+}
+else {
+   echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close()
+
+?>
